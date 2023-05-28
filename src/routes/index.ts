3612d7d9709a7cfa08ec6736baf6ca8042a0b416
AmeMeida <ame.pistache@gmail.com>
2023-05-28 17:22:45 -0300

    simple debug routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,4 +6,16 @@ router.get("/", (_, res) => {
   res.send("Hello World!!");
 });
 
+router.get("/status", (_, res) => {
+  res.json({ status: "OK" });
+});
+
+router.get("/ping", (_, res) => {
+  res.send("pong");
+});
+
+router.get("/brew_coffee", (_, res) => {
+  res.status(418).send("I'm a teapot");
+});
+
 export default router;
